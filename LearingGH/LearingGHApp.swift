292bfb9c33fa2808982877b4ebecc479c6f993ec
Willem Jacobs <wamj58@gmail.com>
2021-04-05 12:28:55 -0500

    Initial Commit

---
+++ LearingGH/LearingGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearingGHApp.swift
+//  LearingGH
+//
+//  Created by Will Jacobs on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct LearingGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
